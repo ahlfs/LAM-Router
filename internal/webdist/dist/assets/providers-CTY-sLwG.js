b8618f6cb522fdb655a949bd3d6e355a80472cbe
Muhamad Alamsyah Ahlul Firdaus <ahlulffirdaus@gmail.com>
2026-09-12 01:33:32 +0700

    style(typography): replace JetBrains Mono with Inter across all components for clean Linear dark aesthetic

---
+++ internal/webdist/dist/assets/providers-CTY-sLwG.js
@@ -0,0 +1,1 @@
+import{j as t,_ as o}from"./index-BDQohUGQ.js";function s(){return t.jsx(o,{})}export{s as component};
--- internal/webdist/dist/assets/providers-DiicDP6i.js
+++
@@ -1,1 +0,0 @@
-import{j as t,_ as o}from"./index-eKPhF9do.js";function s(){return t.jsx(o,{})}export{s as component};
